7f468771b1fcdd285d3a0625ea476f0fe68a4746
kyra-moore21 <kyra.moore21@icloud.com>
2024-09-20 16:12:29 -0400

    swapped database connection to posgress instead of sql sever, used select in get methods to stop infinite looping

--- ProcrastinatorBackend/obj/Debug/net8.0/ProcrastinatorBackend.AssemblyInfo.cs
+++ ProcrastinatorBackend/obj/Debug/net8.0/ProcrastinatorBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProcrastinatorBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c11dc20525277963defe950544685d514c6e659b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10791d03f3bff6785e2d6d7a15a047ddaddd8eb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProcrastinatorBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProcrastinatorBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
